Fixed Regression formula mistake

regression.py
@@ -13,9 +13,11 @@ class KernelRidgeRegression():
         self.n = X_train.shape[0]
         self.X_train = X_train
         self.K_train = self.kernel(X_train, X_train, self.kargs)
+        #print(self.K_train)
         self.alpha = np.dot(
             np.linalg.inv(self.K_train +
-                          lamb * np.identity(self.n, dtype=np.float)), Y_train)
+                          lamb * self.n * np.identity(self.n, dtype=np.float)), Y_train)
+        #print(self.alpha)
         return self.predict_vals(X_train, self.K_train)
 
     # Label prediction function
@@ -27,6 +29,7 @@ class KernelRidgeRegression():
     def predict_vals(self, X_test, K_test=None):
         if K_test is None:
             K_test = self.kernel(self.X_train, X_test, self.kargs)
+        #print(K_test)
         Y_vals = np.asarray(np.dot(self.alpha, K_test)).reshape(-1)
         Y_pred = np.sign(Y_vals).astype(int)
         return Y_pred, Y_vals