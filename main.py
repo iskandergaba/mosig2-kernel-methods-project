404b6e639c5d06ce941a2b3cdccdc00fa648d56b
Hadi Dayekh <hadi.dayekh@grenoble-inp.org>
2021-02-08 10:56:14 +0100

    First attempt at separating the files and saving a model per file - NOT TESTED YET'

--- main.py
+++ main.py
@@ -151,18 +151,7 @@ def ksvm(lamb, kernel, args, save_model=False, read_mat=False):
         df.to_csv('data_processed/Yte.csv', index=True, index_label='Id')
 
 
-def _krr_alphabetic(lamb, kernel, args, save_model=False):
-    Xtr, Ytr, Xte = preproess(numerical=False)
-
-    # Line for correctness testing purposes
-    #Xtr, Ytr = Xtr[:1000], Ytr[:1000]
-
-    # Split the data into training and validation sets
-    X_train, X_val, Y_train, Y_val = util.train_test_split(Xtr,
-                                                           Ytr,
-                                                           test_size=0.2,
-                                                           random=True)
-
+def _krr_alphabetic(X_train, X_val, Y_train, Y_val, Xtr, Ytr, Xte, lamb, kernel, args, index, save_model=False):
     # Initialize model
     model = regression.KernelRidgeRegression(kernel, args)
 
@@ -174,12 +163,10 @@ def _krr_alphabetic(lamb, kernel, args, save_model=False):
         print('Final model accuracy over training data:', acc)
 
         # Save test results
-        Xte = pd.read_csv('data_processed/Xte.csv', delimiter=',')
-        Xte = Xte.drop('Id', axis=1).to_numpy()
         Y_pred = model.predict(Xte).ravel()
         Y_pred[Y_pred == -1] = 0
         df = pd.DataFrame(data={'Bound': Y_pred})
-        df.to_csv('data_processed/Yte.csv', index=True, index_label='Id')
+        df.to_csv('data_processed/Yte'+index+'.csv', index=True, index_label='Id')
 
     else:
         # Fit training data
@@ -191,7 +178,7 @@ def _krr_alphabetic(lamb, kernel, args, save_model=False):
         Y_pred, _ = model.predict_vals(X_val)
         acc = np.sum(Y_val == Y_pred) / Y_pred.shape[0]
         print('Accuracy over testing data:', acc)
-        print(Y_pred)
+        return acc
 
 
 def _krr_numerical(lamb, kernel, args, save_model=False, read_mat=False):
@@ -250,14 +237,14 @@ def krr_rbf(lamb, sigma, save_model=False, read_mat=False):
                           read_mat=read_mat)
 
 
-def krr_spectrum(lamb, k, save_model=False):
+def krr_spectrum(X_train, X_val, Y_train, Y_val, Xtr, Ytr, Xte, lamb, k, index, save_model=False):
     print('Spectrum Kernel Ridge Regression')
-    _krr_alphabetic(lamb, kernels.spectrum, [k], save_model=save_model)
+    _krr_alphabetic(X_train, X_val, Y_train, Y_val, Xtr, Ytr, Xte, lamb, kernels.spectrum, [k], index, save_model=save_model)
 
 
-def krr_mismatch(lamb, k, m, save_model=False):
+def krr_mismatch(X_train, X_val, Y_train, Y_val, Xtr, Ytr, Xte, lamb, k, m, index, save_model=False):
     print('Mismatch Kernel Ridge Regression')
-    _krr_alphabetic(lamb, kernels.mismatch, [k, m], save_model=save_model)
+    _krr_alphabetic(X_train, X_val, Y_train, Y_val, Xtr, Ytr, Xte, lamb, kernels.mismatch, [k, m], index, save_model=save_model)
 
 
 # Using our older RBF simply isn't working
@@ -265,34 +252,48 @@ def krr_mismatch(lamb, k, m, save_model=False):
 #ksvm(100, kernels.rbf_svm, [5], save_model=True, read_mat=False)
 
 # In case we want to conduct a grid search
-lambdas = [0.01, 0.1, 1, 10]
 sigmas = [0.005, 0.05, 0.5, 1, 3, 5, 7, 10]
-ks = [5, 6, 7, 8, 9, 10]
+lambdas = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.1, 0.5, 1]
+ks = [5, 6, 7, 8, 9]
 
-trials, best_acc, best_params = 10, 0, []
-'''
-for lamb in lambdas:
-    for sigma in sigmas:
-        #for k in ks:
-        print("Lambda = {0}, sigma = {1}".format(lamb, sigma))
-        #print("Lambda = {0}, k = {1}".format(lamb, k))
-        sum = 0
-        for i in range(trials):
-            print('Trial', i + 1)
-            sum += krr_rbf(lamb, sigma, save_model=False, read_mat=True)
-            #krr_spectrum(lamb, k, save_model=False)
-        acc = sum / trials
-        print("Average testing accuracy: {0}\n".format(acc))
-        # Update the best accuracy and parameters
-        if best_acc < acc:
-            best_acc = acc
-            best_params = [lamb, sigma]
-            #best_params = [lamb, k]
+trials, best_params = 1, [[],[],[]]
 
-print("Best parameters:\nLambda = {0}\nSigma = {1}".format(best_params[0], best_params[1]))
-print("Best parameters:\nLambda = {0}\nK = {1}".format(best_params[0], best_params[1]))
-krr_rbf(best_params[0], best_params[1], save_model=True, read_mat=True)
-'''
+
+
+for index in range(3):
+    best_acc = 0
+    Xtr, Ytr, Xte = preproess(index, numerical=False)
+    # Split the data into training and validation sets
+    X_train, X_val, Y_train, Y_val = util.train_test_split(Xtr,
+                                                           Ytr,
+                                                           test_size=0.2,
+                                                           random=True)
+
+    
+    for lamb in lambdas:
+        #for sigma in sigmas:
+        for k in ks:
+            #print("Lambda = {0}, sigma = {1}".format(lamb, sigma))
+            print("Lambda = {0}, k = {1}".format(lamb, k))
+            sum = 0
+            for i in range(trials):
+                print('Trial', i + 1)
+                #sum += krr_rbf(lamb, sigma, save_model=False, read_mat=True)
+                sum += krr_spectrum(X_train, X_val, Y_train, Y_val, Xtr, Ytr, Xte, lamb, k, index, save_model=False)
+            acc = sum / trials
+            print("Average testing accuracy: {0}\n".format(acc))
+            # Update the best accuracy and parameters
+            if best_acc < acc:
+                best_acc = acc
+                #best_params[i] = [lamb, sigma]
+                best_params[i] = [lamb, k]
+
+    print("Best parameters:\nLambda = {0}\nSigma = {1}".format(best_params[index][0], best_params[index][1]))
+    #print("Best parameters:\nLambda = {0}\nK = {1}".format(best_params[index][0], best_params[index][1]))
+    #krr_rbf(best_params[0], best_params[1], save_model=True, read_mat=True)
+    krr_spectrum(X_train, X_val, Y_train, Y_val, Xtr, Ytr, Xte, lamb, k, index, save_model=True)
+
+
 # Sample model calls
 #krr_rbf(1, 0.05, save_model=True, read_mat=True)
 #krr_linear(0.01, 0.5, save_model=True, read_mat=True)
