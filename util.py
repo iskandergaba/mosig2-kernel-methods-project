ca9170ce8c8f4e16f157b78ca8050b56d7029d54
Hadi Dayekh <hadi.dayekh@grenoble-inp.org>
2021-02-08 16:55:46 +0100

    modified util so that non-random test is the last bit not the first

--- util.py
+++ util.py
@@ -4,6 +4,6 @@ import numpy as np
 def train_test_split(X, Y, test_size=0.2, random=True):
     size = int(test_size * X.shape[0])
     all = np.arange(0, X.shape[0])
-    test = np.random.choice(X.shape[0], size=size, replace=False) if random else list(range(size))
+    test = np.random.choice(X.shape[0], size=size, replace=False) if random else list(range(X.shape[0]-size, X.shape[0]))
     train = np.where(np.in1d(all, test, invert=True))[0]
     return X[train], X[test], Y[train], Y[test]
